dcc234977dc244a0cd09a7237dccf5f3fa37a56c
robi393 <34414429+robi393@users.noreply.github.com>
2017-12-10 12:07:54 +0100

    Correcting mistakes

--- ImageProc/obj/Debug/ImageProc_Content.g.i.cs
+++ ImageProc/obj/Debug/ImageProc_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("histogram/red.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/default.jpg")]
 
 
